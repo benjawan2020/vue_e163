e37a1f33f4aa5114f8c17aa2ceb78085919f25b8
benjawan2020 <ita5907@ccollege.ac.th>
2020-07-10 15:02:22 +0700

    login

--- pages/login.vue
+++ pages/login.vue
@@ -1,12 +1,20 @@
 <template>
-   <v-card>
-     <v-card-title>
-       <h1>Login</h1>
-     </v-card-title>
+   <v-card color="deep-purple darken-3">
+      <center>
+        <v-icon size="120" color="yellow">grade</v-icon>
+        <h1>Login</h1>
+      </center>
+
+
      <v-card-text>
        <v-form>
          <v-text-field  prepend-icon="mdi-account-circle" label="Username" />
-         <v-text-field  prepend-icon="https" append-icon="visibility_off" label="Password" />
+         <v-text-field 
+          :type="showPassword ? 'text' : 'password'" 
+            label="Password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"/>
        </v-form>
      </v-card-text>
      <v-card-actions>
@@ -17,3 +25,13 @@
      </v-card-actions>
    </v-card>
 </template>
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      showPassword: false
+    }
+  }
+}
+</script>
