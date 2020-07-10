5d8163b50988b46ec18cea510c2f983377fd998e
benjawan2020 <ita5907@ccollege.ac.th>
2020-07-10 13:51:43 +0700

    login

---
+++ pages/login.vue
@@ -0,0 +1,19 @@
+<template>
+   <v-card>
+     <v-card-title>
+       <h1>Login</h1>
+     </v-card-title>
+     <v-card-text>
+       <v-form>
+         <v-text-field  prepend-icon="mdi-account-circle" label="Username" />
+         <v-text-field  prepend-icon="https" append-icon="visibility_off" label="Password" />
+       </v-form>
+     </v-card-text>
+     <v-card-actions>
+        <nuxt-link to="/Register">
+            <v-btn color="success">REGISTER</v-btn> </nuxt-link>
+             <v-spacer></v-spacer>
+            <v-btn color="primary">LOGIN</v-btn>
+     </v-card-actions>
+   </v-card>
+</template>
